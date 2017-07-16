d518e85195c10f627726b891e4b3e79f1e9a9a1c
DINESHDAF <dineshdf34@gmail.com>
2017-07-16 23:43:01 +0530

    Updated for input type file

--- js/main.js
+++ js/main.js
@@ -12,21 +12,73 @@ function init() {
 $(document).ready(function () {
   $('select').material_select();
 });
+
 function closeWarning() {
   var warnContent = document.getElementsByClassName("warn-content")[0];
   warnContent.classList.add("hide");
 }
 
-function checkfile(sender) {
+function checkfileDown(sender) {
+  var fileInputDown = document.getElementById('fileInputDownload');
+  var fileDisplayAreaDown = document.getElementById('fileDisplayAreaDownload');
+  var fileDown = fileInputDown.files[0];
   var validExts = new Array(".txt");
   var fileExt = sender.value;
   fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
   if (validExts.indexOf(fileExt) < 0) {
     alert("You selected {*" + fileExt + "} file, which is not a valid input file for this tool.\nPlease select valid {*" + validExts.toString() + "} file.");
     $(sender).val("");
+    fileDisplayAreaDown.innerText = "";
     return false;
-  } else
-    return true;
+  } else {
+    var readerDown = new FileReader();
+    readerDown.onload = function (e) {
+      fileDisplayAreaDown.innerText = this.result;
+    }
+    readerDown.readAsText(fileDown);
+    console.log(fileDisplayAreaDown.textContent);
+  }
+}
+
+function checkfileExp(sender) {
+  var fileInputExp = document.getElementById('fileInputExport');
+  var fileDisplayAreaExp = document.getElementById('fileDisplayAreaExport');
+  var fileExp = fileInputExp.files[0];
+  var validExts = new Array(".txt");
+  var fileExt = sender.value;
+  fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
+  if (validExts.indexOf(fileExt) < 0) {
+    alert("You selected {*" + fileExt + "} file, which is not a valid input file for this tool.\nPlease select valid {*" + validExts.toString() + "} file.");
+    $(sender).val("");
+    fileDisplayAreaExp.innerText = "";
+    return false;
+  } else {
+    var readerExp = new FileReader();
+    readerExp.onload = function (e) {
+      fileDisplayAreaExp.innerText = this.result.split('\n');
+    }
+    readerExp.readAsText(fileExp);
+    console.log(fileDisplayAreaExp.innerText);
+  }
+}
+
+function getTextOut() {
+  var fileInput = document.getElementById('fileInput');
+  var fileDisplayArea = document.getElementById('fileDisplayArea');
+  var file = fileInput.files[0];
+  var textType = /text.*/;
+
+  if (file.type.match(textType)) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      fileDisplayArea.innerText = reader.result;
+    }
+
+    reader.readAsText(file);
+  } else {
+    fileDisplayArea.innerText = "File not supported!"
+  }
 }
 
 var inputTable = document.getElementById("table");
@@ -42,11 +94,11 @@ for (var i = 0; i < myInputLen; i++) {
 for (var i = 0; i < myOutputLen; i++) {
   getReply[i] = document.getElementById("reply" + i);
 }
-var recSen = document.getElementsByClassName("rec-sen")[0];
+
 function myFunction() {
-  var getReplyType = document.getElementById("getReplyType");
-  var getSelectedValue = getReplyType.options[getReplyType.selectedIndex].value;
-  var getTypeOptionLen = getReplyType.options.length;
+  var getCommandType = document.getElementById("getCommandType");
+  var getSelectedValue = getCommandType.options[getCommandType.selectedIndex].value;
+  var getTypeOptionLen = getCommandType.options.length;
   document.getElementById("empty-fields").innerHTML = " Please fill all the required fields to get output.";
   getInput[0].classList.remove("hide");
   if (getSelectedValue === "opt_0") {
@@ -56,17 +108,13 @@ function myFunction() {
     }
   } else if (getSelectedValue === "opt_1") {
     for (var i = 1; i < myInputLen; i++) {
-      getInput[i].classList.add("hide");
-    }
-  } else if (getSelectedValue === "opt_2") {
-    for (var i = 1; i < myInputLen; i++) {
-      if (i === 3 || i === 5) {
+      if (i === 2) {
         getInput[i].classList.remove("hide");
       } else {
         getInput[i].classList.add("hide");
       }
     }
-  } else if (getSelectedValue === "opt_3" || getSelectedValue === "opt_4" || getSelectedValue === "opt_5" || getSelectedValue === "opt_7" || getSelectedValue === "opt_8") {
+  } else if (getSelectedValue === "opt_2") {
     for (var i = 1; i < myInputLen; i++) {
       if (i === 3) {
         getInput[i].classList.remove("hide");
@@ -74,17 +122,9 @@ function myFunction() {
         getInput[i].classList.add("hide");
       }
     }
-  } else if (getSelectedValue === "opt_6") {
+  } else if (getSelectedValue === "opt_3") {
     for (var i = 1; i < myInputLen; i++) {
-      if (i === 1 || i === 5) {
-        getInput[i].classList.add("hide");
-      } else {
-        getInput[i].classList.remove("hide");
-      }
-    }
-  } else if (getSelectedValue === "opt_9") {
-    for (var i = 1; i < myInputLen; i++) {
-      if (i % 3 === 0) {
+      if (i === 4) {
         getInput[i].classList.remove("hide");
       } else {
         getInput[i].classList.add("hide");
@@ -103,9 +143,9 @@ function myFunction() {
 }
 
 function showReply() {
-  var getReplyType = document.getElementById("getReplyType");
-  var getSelectedIndex = getReplyType.selectedIndex;
-  var getTypeOptionLen = getReplyType.options.length;
+  var getCommandType = document.getElementById("getCommandType");
+  var getSelectedIndex = getCommandType.selectedIndex;
+  var getTypeOptionLen = getCommandType.options.length;
   if (getSelectedIndex === 0) {
     getReply[0].classList.remove("hide");
   } else {
@@ -129,9 +169,9 @@ function assignCopy() {
 }
 
 function copying() {
-  var getReplyType = document.getElementById("getReplyType");
-  var getSelectedIndex = getReplyType.selectedIndex;
-  var getTypeOptionLen = getReplyType.options.length;
+  var getCommandType = document.getElementById("getCommandType");
+  var getSelectedIndex = getCommandType.selectedIndex;
+  var getTypeOptionLen = getCommandType.options.length;
   var node = document.getElementsByClassName('getOutput');
   for (var i = 0; i < getTypeOptionLen; i++) {
     if (i === getSelectedIndex) {
